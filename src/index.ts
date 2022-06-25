e49da84d1f8ade7bf6cc8cd28f36e8fe9c1547d8
coderken <jk0910948659@gmail.com>
2022-06-25 13:19:27 +0800

    update

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,4 @@
-// 引入样式
 import './style/index.less';
-// import Food from "./modules/Food";
-
-// const food = new Food();
-// console.log(food.X, food.Y);
-// food.change();
-// console.log(food.X, food.Y);
-
 import GameControl from "./modules/GameControl";
 
 const gameControl = new GameControl();
